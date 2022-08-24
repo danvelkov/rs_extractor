849a55821b67523c35700b73610810016bdb7917
danvelkov <y.vel.yordanov@gmail.com>
2022-08-24 13:27:00 +0300

    minor fix #6

--- rs_extractor.R
+++ rs_extractor.R
@@ -78,6 +78,7 @@ records <-
 # extracting the annotation data containing id, chr, positions 
 # and adding link to existing reference SNPs or clinical significance Clinvar reference
 foreach (row_count = 1:nrow(records), .packages = 'filelock') %dopar% {
+  print("in")
   line <- c()
   
   elem_name <- records[row_count, 3]
