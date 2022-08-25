7542846443c245c0f0b4d825f37c2553adc6d47d
danvelkov <y.vel.yordanov@gmail.com>
2022-08-25 10:24:58 +0300

    minor fix #47

--- rs_extractor.R
+++ rs_extractor.R
@@ -123,9 +123,9 @@ print(chromosome_files_dir)
 foreach (file = 1:length(chromosome_files_dir)) %do% {
   print(chromosome_files_dir[file][[1]])
   
-  file_name <- paste(dirname(output_file),
+  file_name <- paste(dirname(chromosome_files_dir[file][[1]]),
                      "/chromosome_accessions/",
-                     basename(output_file), "_chr", chromosome[chr], "_accession", sep="")
+                     basename(chromosome_files_dir[file][[1]]), "_chr", chromosome[file], "_accession", sep="")
   
   command <- paste(
     "bcftools query -f '%ID\n'",
